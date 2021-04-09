c61ea90090e6057f0130734ab1c05f7f505f892f
Raspberry Pi <raspberrypisig@gmail.com>
2021-04-09 18:59:53 +1000

    C++ example

---
+++ C++examples/001-C++helloworld/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+
+    std::cout << "Hello there. Enter a string here:" ;
+    std::string user_input;
+    std::cin >> user_input;
+    std::cout << "You entered: " << user_input << "\n";
+    
+}
